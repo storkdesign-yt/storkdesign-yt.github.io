70b9c91dc6a0a0120c00075b42e3e49b9aed2ec0
Stork <storkgaming@o2.pl>
2025-06-01 16:18:34 +0200

    e

--- website/script.js
+++ website/script.js
@@ -63,7 +63,7 @@ if (!isActive) {
 
 
 var typed = new Typed(".auto-type", {
-  strings: ["wysoką jakość.", "krótki czas oczekiwania.", "dopracowane szczegóły."],
+  strings: ["Jakość.", "Czas Realizaji.", "Szczegóły."],
   typeSpeed: 50,
   backSpeed: 50,
   startDelay: 2000,
@@ -72,71 +72,11 @@ var typed = new Typed(".auto-type", {
 });
 
 const opinionsData = [
-  {
-    image: "images/discord_avatars/veeon.png",
-    text: "2000/2000",
-    nickname: ".veeon"
-  },
-  {
-    image: "images/discord_avatars/kamkil.png",
-    text: "Wyśmienita realizacja, bezproblemowa płatność! Generalnie 10/10",
-    nickname: "kamkil"
-  },
-  {
-    image: "images/discord_avatars/dawid6157.png",
-    text: "Polecam good grafik",
-    nickname: "dawid6157"
-  },
-  {
-    image: "images/discord_avatars/ziomek.png",
-    text: "Polecam Bardzo dobry grafik 10/10",
-    nickname: "zισмєк#1728"
-  },    
-  {
-    image: "images/discord_avatars/edzzi.png",
-    text: "Bardzo ale to bardzo polecam zajebisty grafik 100/10",
-    nickname: "edzzi"
-  },   
-  {
-    image: "images/discord_avatars/_joinme.png",
-    text: "Polecam. Jakość wykonania jest znacznie wyższa niż się spodziewałam 10/10",
-    nickname: "_joinme"
-  },   
-  {
-    image: "images/discord_avatars/michi0851.png",
-    text: "10/10",
-    nickname: "michi0851"
-  },   
-  {
-    image: "images/discord_avatars/ilg.filip.png",
-    text: "zajebisty montaż oraz wykonanie avataru i banneru ❤️ 11/10",
-    nickname: "ilg.filip"
-  },   
-  {
-    image: "images/discord_avatars/filipeeeek_.png",
-    text: "100/10 zajebiste dziekiii",
-    nickname: "filipeeeek_"
-  },   
-  {
-    image: "images/discord_avatars/og.vexx.png",
-    text: "1000000000/10 Bardzo ciepło polecam tego użytkownika zajebista grafika",
-    nickname: "og.vexx"
-  },   
-  {
-    image: "images/discord_avatars/matsoor.png",
-    text: "Polecam!!!",
-    nickname: "matsoor"
-  },   
-  {
-    image: "images/discord_avatars/buteleczkagra.png",
-    text: "10/10",
-    nickname: "buteleczkagra"
-  },   
-  {
-    image: "images/discord_avatars/facejoker.png",
-    text: "Gittt",
-    nickname: "facejoker"
-  },   
+    {
+      image: "images/discord_avatars/dreibon.png",
+      text: "Szybka i profesjonalna robota.",
+      nickname: "Dreibon"
+    },  
 ];
 
 let currentOpinionIndex = 0;
